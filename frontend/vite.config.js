067e9b989b5ea70037bdd9b65a49d5963cc945a6
rohan44942 <rohannooniwal39@gmail.com>
2026-05-25 23:43:49 +0530

    vite updated for accessing this url publicaly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['ethara-frontend-production-a49d.up.railway.app']
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['ethara-frontend-production-a49d.up.railway.app']
+  }
 })
